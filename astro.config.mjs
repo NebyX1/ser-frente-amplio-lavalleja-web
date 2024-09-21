a91d9c3b244f470892984060378de9be333d2809
NebyX1 <blackcat_neme@hotmail.com>
2024-09-21 19:27:06 -0300

    se quitó un paquete problemático llamado path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+
 import react from '@astrojs/react';
-import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  vite: {
-    resolve: {
-      alias: {
-        '@': path.resolve('./src')  // Define '@' como alias para 'src'
-      }
-    }
-  }
+  integrations: [tailwind(), react()]
 });
